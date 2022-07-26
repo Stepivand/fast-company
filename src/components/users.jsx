3d58947fa393d29f3bd504189db36fba7d9f2444
Ivanov Stepan <stepivand@gmail.com>
2022-07-26 20:44:27 +0500

    my task 1

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,75 +1,76 @@
 import { logDOM } from '@testing-library/react'
-import React, { useState } from "react";
-import api from "../api"
+import React, { useState } from 'react'
+import api from '../api' 
 
 const Users = () => {
-    const [users, setUsers] = useState(api.users.fetchAll());
+  const [users, setUsers] = useState(api.users.fetchAll())
+ 
+  const handleDelete = (userId) => {
+    setUsers(prevState => prevState.filter(user => user !== userId))
+  }
 
-    const handleDelete = (userId) => {
-        setUsers(users.filter((user) => user._id !== userId));
-    };
+  const getQquality = (user) => {
+    let color = "badge bg-"
+    return user.qualities.map((quality) => (
+      <span className = {color + quality.color}>{quality.name}</span>
+    ))
+  }
 
-    const renderPhrase = (number) => {
-        const lastOne = Number(number.toString().slice(-1));
-        if (number > 4 && number < 15) return "человек тусанет";
-        if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
-        if (lastOne === 1) return "человек тусанет";
-        return "человек тусанет";
-    };
-
+  const renderPhrase = () => {
     return (
-        <>
-            <h2>
-                <span
-                    className={"badge " + (users.length > 0 ? "bg-primary" : "bg-danger")}
-                >
-                    {users.length > 0
-                        ? `${users.length + " " + renderPhrase(users.length)} с тобой сегодня`
-                        : "Никто с тобой не тусанет"}
-                </span>
-            </h2>
+      users.length !== 0 && 
+      users.map((user) => (
 
-            {users.length > 0 && (
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Имя</th>
-                            <th scope="col">Качества</th>
-                            <th scope="col">Профессия</th>
-                            <th scope="col">Встретился, раз</th>
-                            <th scope="col">Оценка</th>
-                            <th />
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map((user) => (
-                            <tr key={user._id}>
-                            <td>{user.name}</td>
-                            <td>
-                                {user.qualities.map((item) => (
-                                    <span className={"badge m-1 bg-" + item.color} key={item._id}>
-                                        {item.name}
-                                    </span>
-                                ))}
-                            </td>
-                            <td>{user.profession.name}</td>
-                            <td>{user.completedMeetings}</td>
-                            <td>{user.rate} /5</td>
-                            <td>
-                                <button
-                                    onClick={() => handleDelete(user._id)}
-                                    className="btn btn-danger"
-                                >
-                                    delete
-                                </button>
-                            </td>
-                        </tr>
-                        ))}
-                    </tbody>
-                </table>
-            )}
-        </>
-    );
-};
+        <tr>
+          <th>{ user.name }</th>
+          <th>{getQquality(user)}</th>
+          <th>{ user.profession.name }</th>
+          <th>{ user.completedMeetings }</th>
+          <th>{ user.rate }</th>
+          <th>
+            <button 
+              className = "btn btn-danger btn-sm m-2"
+              onClick = {() => handleDelete(user)}
+            >
+              delete
+            </button>
+          </th>
+        </tr>   
+      ))
+    )        
+  }
 
-export default Users;
+  if(users.length !== 0) {
+    return (
+      <>
+        <h2>
+          <span className = "badge bg-primary">{users.length} тусуются с тобой сегодня</span>
+        </h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Имя</th>
+              <th>Качества</th>
+              <th>Професссия</th>
+              <th>Встретился, раз</th>
+              <th>Оценка</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>{renderPhrase()}</tbody>
+        </table>
+      </>
+    )
+  }
+
+  return (   
+     <>
+        <h2>
+          <span className = "badge bg-danger">Никто с тобой не тусанет</span>
+        </h2>
+    </>
+  )
+
+}
+
+export default Users
